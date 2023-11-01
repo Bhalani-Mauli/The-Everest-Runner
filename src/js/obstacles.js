e06411c7ed7921fbc07fa01712fd08e64c75bc11
Bhalani-Mauli <maulibhalani13@gmail.com>
2023-11-01 12:27:24 +0100

    create obstacle file

---
+++ src/js/obstacles.js
@@ -0,0 +1,27 @@
+class Obstacle {
+  constructor(gameScreen) {
+    this.gameScreen = gameScreen;
+    this.left = Math.floor(Math.random() * 800 + 400);
+    this.top = 580;
+    this.width = 100;
+    // this.height = 150;
+    this.element = document.createElement("img");
+    this.element.src = "../../assets/obstacle/snowman.svg";
+
+    this.element.style.position = "absolute";
+    this.element.style.zIndex = 3;
+    this.element.style.width = `${this.width}px`;
+    // this.element.style.height = `${this.height}px`;
+    this.element.style.left = `${this.left}px`;
+    this.element.style.top = `${this.top}px`;
+
+    this.gameScreen.appendChild(this.element);
+  }
+  updatePosition() {
+    this.element.style.left = `${this.left}px`;
+    this.element.style.top = `${this.top}px`;
+  }
+  move() {
+    this.updatePosition();
+  }
+}
